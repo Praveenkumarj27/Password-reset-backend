c926aa5bc5346c7fd763ebb7c73ffbda4d0002ff
praveenkumarj27 <jpraveenkumar263@gmail.com>
2022-09-22 09:55:18 +0530

    link

--- routes/users.js
+++ routes/users.js
@@ -77,7 +77,7 @@ router.post("/resetpassword", async function (request, response) {
       let mailid = request.body.email;
       let rString = randomstring.generate(7);
       let link =
-        "https://password-reset-front-end.vercel.app/reset-password-page";
+        "https://password-reset-taskk.netlify.app/reset-password-page";
       await db
         .collection("users")
         .updateOne({ email: mailid }, { $set: { rString: rString } });
